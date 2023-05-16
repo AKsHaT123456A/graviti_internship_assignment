779028d6406be5cdf9efdbe923783488442e3044
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-05-16 20:21:44 +0530

    Fixed Changes

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -7,9 +7,20 @@ import {
   InputGroup,
   InputLeftElement,
   SkeletonText,
+  border,
 } from "@chakra-ui/react";
-import { FaTimes, FaMapMarkerAlt, FaCircle, FaPlus, FaMinus, FaPlusCircle, FaMinusCircle } from "react-icons/fa";
-
+import {
+  FaTimes,
+  FaMapMarkerAlt,
+  FaCircle,
+  FaPlus,
+  FaMinus,
+  FaPlusCircle,
+  FaMinusCircle,
+} from "react-icons/fa";
+import  LOGO from "../assests/ellipse.png" 
+import  LOGO2 from "../assests/room.png" 
+import  LOGO1 from "../assests/e20.png" 
 import {} from "react-icons";
 import "./main.css";
 import {
@@ -18,7 +29,7 @@ import {
   Autocomplete,
   DirectionsRenderer,
 } from "@react-google-maps/api";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 const center = { lat: 21, lng: 78 };
 function Main() {
   const { isLoaded } = useJsApiLoader({
@@ -26,24 +37,35 @@ function Main() {
     libraries: ["places"],
     region: "IN",
   });
-
+  const waypointsInputRefs=useRef([]);
   const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [searchResult, setSearchResult] = useState('')
   const [directionsResponse, setDirectionsResponse] = useState(null);
   const [distance, setDistance] = useState("");
   const [duration, setDuration] = useState("");
+  const [indexes,setIndexes]=useState();
   const [originVal, setOriginVal] = useState("");
   const [destinationVal, setDestinationVal] = useState("");
-  const [inputs, setInputs] = useState([]);
-  const waypointsInputRefs = useRef([]);
-  const [error, setError] = useState("");
+  const [inputs, setInputs] = useState([""]);
+  const [stops, setStops] = useState([]);
+  const [error,setError] = useState("");
+  const [stops1,setStops1] = useState([]);
+  const [waypoints,setWaypoints] = useState([]);
   const [formErrors, setFormErrors] = useState({});
-  const [stop1,setStop1]=useState([]);
+  const [check, setcheck] = useState(false);
+  const [stopCopy, setStopCopy] = useState();
+  let stops1Copy=[];
+  useEffect(()=>{
+    setStopCopy(stops);
+    console.log(stops);
+  },[check,stops]);
+
   let errorObj = {};
   /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef();
+  const stopRef=useRef();
   /** @type React.MutableRefObject<HTMLInputElement> */
   const destinationRef = useRef();
-  const stopsRef = useRef();
   if (!isLoaded) {
     return <SkeletonText />;
   }
@@ -52,32 +74,44 @@ function Main() {
   };
   const handleInputChange = (event, index) => {
     const newInputs = [...inputs];
-    newInputs[index] = event.target.value;
+    newInputs[index] = waypointsInputRefs.current.value;
+    console.log("heelo");
     console.log(newInputs);
     setInputs(newInputs);
-    console.log(newInputs);
   };
   function addInput(e) {
     e.preventDefault();
     setInputs([...inputs, ""]);
-    console.log(inputs);
-  }
-  const handleStopChange=(e)=>{
-    setInputs(e.target.value)
   }
   function removeInput(index) {
     const newInputs = [...inputs];
     newInputs.splice(index, 1);
     setInputs(newInputs);
-    console.log(newInputs);
+  }
+  function onLoad(autocomplete) {
+    setSearchResult(autocomplete);
+  }
+  function onPlaceChanged() {
+    if (searchResult != null) {
+      setcheck(true);
+      const place = searchResult.getPlace(); 
+      const name = place.name;
+      setStops([...stops,name]);
+      stops1Copy=stops;
+      stopCopy[indexes]=name;
+      console.log(`Name: ${name}`);
+      setStops1(stops1Copy);
+      console.log(stops1);
+      setcheck(false);
+    } else {
+      alert("Please enter text");
+    }
   }
   async function calculateRoute() {
     try {
       setError(false);
       const origin = originRef.current.value;
       const destination = destinationRef.current.value;
-      // const stop=stopsRef.current.value;
-      // setStop(stop);
       const originName = origin.split(",")[0];
       const destinationName = destination.split(",")[0];
       setOriginVal(originName);
@@ -100,7 +134,7 @@ function Main() {
         errorObj.destinationError = false;
         setFormErrors(errorObj);
       }
-      const waypoints = inputs.map((stop) => ({
+      const waypoints = stops.map((stop) => ({
         location: stop,
         stopover: true,
       }));
@@ -113,6 +147,7 @@ function Main() {
         // eslint-disable-next-line no-undef
         travelMode: google.maps.TravelMode.DRIVING,
       });
+      
       let totalDistance = 0;
       for (let i = 0; i < results.routes[0].legs.length; i++) {
         totalDistance += results.routes[0].legs[i].distance.value;
@@ -133,7 +168,7 @@ function Main() {
       }
       const days = Math.floor(durationInSeconds / 86400);
       const hours = Math.floor((durationInSeconds % 86400) / 3600);
-      setDuration(`${days} days ${hours} hours`);
+      setDuration(`${days} day/s ${hours} hours`);
       setDirectionsResponse(results);
       setDistance(`${(totalDistance / 1000).toFixed(0)} km`);
     } catch (error) {
@@ -148,10 +183,10 @@ function Main() {
     setDirectionsResponse(null);
     setDistance("");
     setDuration("");
-    setInputs([]);
+    setStops([]);
     originRef.current.value = "";
     destinationRef.current.value = "";
-    stopsRef.current.value = "";
+    stopRef.current.value = "";
   }
   return (
     <>
@@ -182,48 +217,29 @@ function Main() {
                         ref={originRef}
                         onChange={(e) => handleOriginChange(e)}
                       />
-                      <InputLeftElement
-                        pointerEvents="none"
-                        children={<FaCircle size={15} color="green" />}
-                      />
+                      <InputLeftElement pointerEvents={"none"}>
+                      <img src={LOGO} alt="logo" width={15} height={15} />
+                      </InputLeftElement>
                     </InputGroup>
-                  </Autocomplete>
+                  </Autocomplete >
                 </div>
                 <Heading as="h6" size="xs" className="heading">
                   Stops
                 </Heading>
-                <div className="field" >
-                  <Autocomplete>
-                    <InputGroup>
-                      <Input
-                        className="stop-field"
-                        type="text"
-                        background={"#FFFF"}
-                        placeholder="Stops"
-
-                        ref={
-                          waypointsInputRefs
-                        }
-                      />
-                      <InputLeftElement
-                        pointerEvents="none"
-                        children={<FaCircle size={15} color="black" />}
-                      />
-                    </InputGroup>
-                  </Autocomplete>
+                <div className="field">
                   {inputs.map((input, index) => (
                     <div key={index} className="stop-field">
-                      <Autocomplete>
+                      <Autocomplete onPlaceChanged={onPlaceChanged} onLoad={onLoad} onClick={e=>setIndexes(index)}>
                         <InputGroup>
                           <Input
                             type="text"
                             background={"#FFFF"}
                             placeholder="Stops"
                             value={input}
-                            onClick={e=>handleInputChange(e)}
-                            // ref={(element) =>
-                            //   waypointsInputRefs.current.push(element)
-                            // }
+                            onClick={(e) => handleInputChange(e)}
+                            ref={(element) =>
+                              waypointsInputRefs.current.push(element)
+                            }
                             onChange={(e) => {
                               handleInputChange(e, index);
                             }}
@@ -236,16 +252,19 @@ function Main() {
                       </Autocomplete>
                     </div>
                   ))}
-                  <div className="buttons" style={{display:"flex",justifyContent:"center"}}>
-                  <button type="click" onClick={addInput} id="addButton">
-                    <FaPlusCircle size={10} /> Add another stop
-                  </button>
-                  <button type="click" onClick={removeInput} id="addButton">
-                    <FaMinusCircle size={10} /> Remove 
-                  </button>
+                  <div
+                    className="buttons"
+                    style={{ display: "flex", justifyContent: "center" }}
+                  >
+                    <button type="click" onClick={addInput} id="addButton">
+                      <FaPlusCircle size={10} /> Add another stop
+                    </button>
+                    <button type="click" onClick={removeInput} id="addButton">
+                      <FaMinusCircle size={10} /> Remove
+                    </button>
                   </div>
                 </div>
-                <Heading as="h6" size="xs" className="heading" >
+                <Heading as="h6" size="xs" className="heading">
                   Destination
                 </Heading>
                 <div className="field">
@@ -267,7 +286,9 @@ function Main() {
                       <InputLeftElement
                         pointerEvents="none"
                         children={<FaMapMarkerAlt />}
-                      />
+                      /> <InputLeftElement pointerEvents={"none"}>
+                      <img src={LOGO2} alt="logo" width={15} height={15} />
+                      </InputLeftElement>
                     </InputGroup>
                   </Autocomplete>
                 </div>
@@ -277,6 +298,7 @@ function Main() {
                 <ButtonGroup>
                   <div className="calcButton-1">
                     <Button
+                     _hover={{ border: "none", outline: "none", boxShadow: "none" }}
                       colorScheme="#1B31A8"
                       type="submit"
                       onClick={calculateRoute}
@@ -287,15 +309,16 @@ function Main() {
                   </div>
                   <div
                     className="calcButton-2"
-                    style={{ display: "flex", alignItems: "center" }}
+                    style={{ display: "flex", alignItems: "center" ,position:"relative", top:"1rem"}}
                   >
                     <IconButton
                       aria-label="center back"
                       borderRadius={"50%"}
                       background={"#1b31a8"}
                       alignItems={"center"}
-                      // position={"relative"}
-                      icon={<FaTimes color="white" />}
+                      position={"relative"}
+                      top={"-1rem"}
+                      icon={LOGO1}
                       onClick={clearRoute}
                     />
                   </div>
@@ -322,7 +345,7 @@ function Main() {
                   </h1>
                 </div>
               </div>
-              <div className="distance-sentence">
+              <div className="distance-sentence" style={{padding:"0.5rem"}}>
                 {distance ? (
                   `The distance between ${originVal} and ${destinationVal} via the seleted route is ${distance}s and the approximate time of shipping is ${duration}.`
                 ) : (
